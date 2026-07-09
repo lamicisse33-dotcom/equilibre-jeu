e350ca934daaacfcb4414ad91eabadfc11e1fff6
lamicisse33-dotcom <lamicisse33@gmail.com>
2026-07-09 15:39:42 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
    - HTML : réseau d'abord (tes mises à jour s'appliquent en ligne), repli cache hors-ligne.
    - Icônes/manifeste : cache d'abord.
    Pense à changer VERSION quand tu déploies une nouvelle version du jeu. */
-const VERSION = "equilibre-v94-72";
+const VERSION = "equilibre-v94-78";
 const SHELL = [
   "./",
   "./index.html",
